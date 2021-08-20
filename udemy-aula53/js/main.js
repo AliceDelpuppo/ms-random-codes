fd6725f1c6b74bf89c61827c39b2ae93e079ec55
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-08-20 17:05:08 -0300

    Primeira modificação

---
+++ udemy-aula53/js/main.js
@@ -0,0 +1,35 @@
+const $insertText = document.querySelector('.insert-here')
+const div = document.createElement('div')
+
+const ELEMENTOS = [
+    {
+        tag: 'p',
+        text: 'Qualquer texto'
+    },
+
+    {
+        tag: 'div',
+        text: 'Qualquer texto2'
+    },
+
+    {
+        tag: 'section',
+        text: 'Qualquer texto3'
+    },
+
+    {
+        tag: 'footer',
+        text: 'Qualquer texto4'
+    }
+]
+
+ELEMENTOS.forEach(function (elemento) {
+    const tag = elemento.tag
+    const text = elemento.text
+
+    const tagCreated = document.createElement(tag)
+    tagCreated.textContent = text
+    div.appendChild(tagCreated)
+});
+
+$insertText.appendChild(div)
