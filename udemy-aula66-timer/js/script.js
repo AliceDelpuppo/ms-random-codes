7020f236881e2e45ff818abcb7fc8636e74ac50b
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-09-04 13:53:20 -0300

    Construção cronômetro

---
+++ udemy-aula66-timer/js/script.js
@@ -0,0 +1,40 @@
+const $buttonStart = document.querySelector('.button-start')
+const $buttonPause = document.querySelector('.button-pause')
+const $buttonReset = document.querySelector('.button-reset')
+
+const $clock = document.querySelector('.clock')
+
+let seconds = 0
+let timer
+
+function getHourFromSeconds(seconds) {
+    const date = new Date(seconds * 1000)
+    return date.toLocaleTimeString('pt-br', {
+        hour12: false,
+        timeZone: 'GMT'
+    })
+}
+
+function startClock() {
+    timer = setInterval(() => {
+        seconds++
+        $clock.textContent = getHourFromSeconds(seconds)
+    }, 1000);
+}
+
+$buttonStart.addEventListener('click', function () {
+    startClock()
+    $clock.classList.remove('paused')
+})
+
+$buttonPause.addEventListener('click', function () {
+    clearInterval(timer)
+    $clock.classList.add('paused')
+})
+
+$buttonReset.addEventListener('click', function(){
+    clearInterval(timer)
+    $clock.textContent = '00:00:00'
+    seconds = 0
+    $clock.classList.remove('paused')
+})
