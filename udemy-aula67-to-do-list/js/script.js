2158015bdae24315305ab404ca93862925214ed1
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-09-05 20:55:06 -0300

    pequenas modificações

--- udemy-aula67-to-do-list/js/script.js
+++ udemy-aula67-to-do-list/js/script.js
@@ -2,11 +2,20 @@ const $inputNewTarefas = document.querySelector('.input-new-tarefas')
 const $buttonAddTarefas = document.querySelector('.button-add-tarefas')
 const $localAddTarefas = document.querySelector('.local-add-tarefas')
 
-function createTarefa(textInput) {
-    const li = createLi()
-    li.textContent = textInput
+// const $buttonRemoveItemTarefa = document.querySelectorAll('.button-remove-item-tarefa')
 
-    $localAddTarefas.appendChild(li)
+function createTarefa(textInput) {
+    const $li = createLi()
+    // li.textContent = textInput
+    const $p = document.createElement('p')
+    const tx = '- ' + textInput
+    const text = document.createTextNode(tx)
+    console.log(text)
+    $p.appendChild(text)
+    $li.appendChild($p)
+    $localAddTarefas.appendChild($li)
+
+    createButtonDelete($li)
 }
 
 function createLi() {
@@ -18,9 +27,29 @@ function clearInput() {
     // $inputNewTarefas.focus()
 }
 
-function createButtonDelete(li) {
+function createButtonDelete($li) {
+    const $divButton = document.createElement('div')
+    $divButton.classList.add('button-generic')
     
+    // $divButton.setAttribute('class', 'button-remove-item-tarefa button-generic')
+    
+    $divButton.classList.add('button-remove-item-tarefa', 'button-generic')
+    $divButton.setAttribute('title', 'Apagar esta tarefa')
+
+
+    const text = document.createTextNode('Remover')
+    $divButton.appendChild(text)
+    $li.appendChild($divButton)
 }
+
+document.addEventListener('click', function(event){
+    const element = event.target
+
+    if(element.classList.contains('button-remove-item-tarefa')){
+        element.parentElement.remove()
+    }
+})
+
 
 $inputNewTarefas.addEventListener('keypress', function (event) {
     // console.log(event)
@@ -28,6 +57,7 @@ $inputNewTarefas.addEventListener('keypress', function (event) {
         const $inputValue = $inputNewTarefas.value
 
         if (!$inputValue) return
+        
         createTarefa($inputValue)
         clearInput()
     }
@@ -38,6 +68,7 @@ $buttonAddTarefas.addEventListener('click', function (event) {
     const $inputValue = $inputNewTarefas.value
 
     if (!$inputValue) return
+    
     createTarefa($inputValue)
     clearInput()
 })
